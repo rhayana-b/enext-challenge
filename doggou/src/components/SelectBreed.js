254c6c6a584545edbfd326955dd37e67cceaa482
rhayana-b <rhayanabarbosa@gmail.com>
2020-06-03 16:10:11 -0300

    integrate SelectBreed with API

--- doggou/src/components/SelectBreed.js
+++ doggou/src/components/SelectBreed.js
@@ -1,14 +1,33 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import api from '../services/api';
 
 const SelectBreed = () => {
+  const [breeds, setBreeds] = useState([]);
+
+  useEffect(() => {
+    api.get('breeds/list/all').then(response => {
+      const allBreeds = response.data.message;
+      setBreeds(Object.keys(allBreeds)); 
+    }).catch(error => {
+      console.log(error);
+    });
+  }, []);
+
+  const renderOptions = () => {
+    return breeds.map(breed => (
+      <option key={breed} value={breed}>
+        { breed.charAt(0).toUpperCase() + breed.slice(1) }
+      </option>
+    ));
+  }
+
   return (
     <div className="comp-wrapper">
     <label for="breeds">Selecione uma raça:</label>
 
     <select name="breeds" id="breeds">
-      <option value="poodle">Poodle</option>
-      <option value="akita">akita</option>
+      <option selected disabled>--</option>
+      { renderOptions() }
     </select>
     </div>
   );
