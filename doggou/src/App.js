892cb67f09b10f5952c897f4438305b58076afce
rhayana-b <rhayanabarbosa@gmail.com>
2020-06-03 13:44:49 -0300

    create Input Breed component

--- doggou/src/App.js
+++ doggou/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import api from './services/api';
+import SelectBreed from './components/SelectBreed';
+import BreedInput from './components/BreedInput';
 
 const App = () => (
-    <div className="App">
-        <h1>Hello</h1>
-    </div>
+  <div className="App">
+    <SelectBreed/>
+    <BreedInput/>
+  </div>
 );
 
-
-
 export default App;
+  
---
+++ doggou/src/components/BreedInput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BreedInput = () => {
+  return (
+    <div>
+      <form action="">
+        <label for="petname">Insira o nome do Pet:</label>
+        <input type="text" id="petname" name="petname"/>
+      </form> 
+    </div>
+  );
+}
+
+export default BreedInput;
