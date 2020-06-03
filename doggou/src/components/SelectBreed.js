c54cf0060135701244ce21b380d3d2eda4d1da3a
rhayana-b <rhayanabarbosa@gmail.com>
2020-06-03 13:36:23 -0300

    create Select Breed component

---
+++ doggou/src/components/SelectBreed.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const SelectBreed = () => {
+  return (
+    <div>
+    <label for="breeds">Selecione uma raça:</label>
+
+    <select name="breeds" id="breeds">
+      <option value="poodle">Poodle</option>
+      <option value="akita">akita</option>
+    </select>
+    </div>
+  )
+}
+
+export default SelectBreed;
+  
