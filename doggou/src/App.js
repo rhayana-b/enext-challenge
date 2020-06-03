2991556bbf158f657877851815451bd2db9a0b21
rhayana-b <rhayanabarbosa@gmail.com>
2020-06-03 12:29:43 -0300

    init app

---
+++ doggou/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import api from './services/api';
+
+const App = () => (
+    <div className="App">
+        <h1>Hello</h1>
+    </div>
+);
+
+
+
+export default App;
